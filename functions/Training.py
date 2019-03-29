5fdd85ef1c094291a2c788f90bad74052d67cb24
lilivalencia21 <lilivalencia.21@gmail.com>
2019-03-29 11:36:52 +0100

    Preliminar cross-validation

--- functions/Training.py
+++ functions/Training.py
@@ -1,9 +1,13 @@
 import time
 
 from numpy import inf
-
+import numpy as np
 from functions.utilities import cross_entropy_wrapper
 import torch
+import operator
+import torch.optim as optim
+from functions.instructions import *
+from functions.nets import *
 
 
 def train_net(train_gen, val_gen, model, max_epochs, optimizer, device):
@@ -71,5 +75,55 @@ def train_net(train_gen, val_gen, model, max_epochs, optimizer, device):
         running_loss = 0.0
 
     print('Finished Training')
+
+
+
+def cross_validation(dataset, sampler, params, patch_shape, folds=4):
+
+    indexes = np.linspace(start=0, stop=len(dataset) - 1, num=len(dataset), dtype='uint8')
+    folds = np.arange(0, 4)
+    num = int(len(indexes) / len(folds))
+
+    # # Iterate over the dataset
+    for i, fold in enumerate(folds):
+        print('=====================================')
+        print('Fold Number ', i + 1)
+        print('=====================================')
+        # Restart the model
+        # model = UNet3D().to(device)
+
+        # Get the indexes of the cases used for training. The rest are for validation
+        val_idx = indexes[i * num:(i + 1) * num]
+        # print('indexes of validation {}'.format(val_idx))
+        train_idx = [index for index in indexes if index not in val_idx]
+        # print('indexes of validation {}'.format(train_idx))
+
+        # Loads training and validation data
+        train_set = operator.itemgetter(*train_idx)(dataset)
+        val_set = operator.itemgetter(*val_idx)(dataset)
+
+        print("Generating training instructions...")
+        instructions_train = generate_instruction(train_set, sampler, patch_shape)
+        train_data = myDataset(train_set, instructions_train)
+        train_gen = torch.utils.data.DataLoader(train_data, **params)
+        print("Generated {} training instructions from {} images".format(len(instructions_train), len(train_set)))
+
+        print("Generating validation instructions...")
+        instructions_val = generate_instruction(val_set, sampler, patch_shape)
+        val_data = myDataset(val_set, instructions_val)
+        val_gen = torch.utils.data.DataLoader(val_data, **params)
+        print("Generated {} validation instructions from {} images".format(len(instructions_val), len(val_set)))
+
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        model = UNet3D()
+        model.to(device)
+        max_epochs = 10
+        optimizer = optim.Adadelta(model.parameters())
+
+        train_net(train_gen, val_gen, model, max_epochs, optimizer, device)
+
+        # Save the model
+        torch.save(model.state_dict(), '/home/liliana/models/net_norm_crossvalidation{}.pth'.format(i))
+
 
 
--- main.py
+++ main.py
@@ -8,18 +8,20 @@ import torch.optim as optim
 from functions.nets import *
 from functions.Training import *
 
+
 data_dir_train = "/home/liliana/Data/train"
 # data_dir_train = "/home/liliana/Brats18TrainingData/"
 
 print("Loading dataset...")
 #load the dataset
 dataset = load_dataset(data_dir_train)
+print('Length of dataset is {}'.format(len(dataset)))
 #Split the dataset in training and validation sets
 train_percentage = 0.8
 train_set, val_set = split_dataset(dataset, train_percentage)
 
 # Parameters for dataloader
-params = {'batch_size': 32,
+params = {'batch_size': 128,
           'shuffle': True,
           'num_workers': 64}
 
@@ -29,30 +31,32 @@ step = (12, 12, 12)
 sampler = UniformSampler(patch_shape, step)
 #sampler = BalancedSampler(patch_shape)
 
-print("Generating training instructions...")
-instructions_train = generate_instruction(train_set, sampler, patch_shape)
-train_data = myDataset(train_set, instructions_train)
-train_gen = torch.utils.data.DataLoader(train_data, **params)
-print("Generated {} training instructions from {} images".format(len(instructions_train), len(train_set)))
+cross_validation(dataset, sampler, params, patch_shape)
 
-print("Generating validation instructions...")
-instructions_val = generate_instruction(val_set, sampler, patch_shape)
-val_data = myDataset(val_set, instructions_val)
-val_gen = torch.utils.data.DataLoader(val_data, **params)
-print("Generated {} validation instructions from {} images".format(len(instructions_val), len(val_set)))
+# print("Generating training instructions...")
+# instructions_train = generate_instruction(train_set, sampler, patch_shape)
+# train_data = myDataset(train_set, instructions_train)
+# train_gen = torch.utils.data.DataLoader(train_data, **params)
+# print("Generated {} training instructions from {} images".format(len(instructions_train), len(train_set)))
+#
+# print("Generating validation instructions...")
+# instructions_val = generate_instruction(val_set, sampler, patch_shape)
+# val_data = myDataset(val_set, instructions_val)
+# val_gen = torch.utils.data.DataLoader(val_data, **params)
+# print("Generated {} validation instructions from {} images".format(len(instructions_val), len(val_set)))
+#
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# model = UNet3D()
+# model.to(device)
+# max_epochs = 10
+# optimizer = optim.Adadelta(model.parameters())
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-model = UNet3D()
-model.to(device)
-max_epochs = 10
-optimizer = optim.Adadelta(model.parameters())
-
-
-print("Training...")
-Train_network = train_net(train_gen, val_gen, model, max_epochs, optimizer, device)
-
-#Save the model
-torch.save(model.state_dict(), "/home/liliana/models/net_norm_overlap.pth")
+#
+# print("Training...")
+# Train_network = train_net(train_gen, val_gen, model, max_epochs, optimizer, device)
+#
+# #Save the model
+# torch.save(model.state_dict(), "/home/liliana/models/net_norm_overlap.pth")
 
 
 
